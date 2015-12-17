16af3363adcf77cceb080c524484fdd8094e62b8
GaxXanh <anhapo11@gmail.com>
2015-12-17 20:27:36 +0700

    Second

---
+++ src/Config/Config.java
@@ -0,0 +1,14 @@
+package Config;
+
+/**
+ * Author: Gác Xanh (phamanh)
+ * Date: 17/12/2015
+ * Class: OOP2
+ * Project: ProjectGame
+ */
+public class Config {
+    public static class WindowProperties{
+        public static final int WINDOW_WIDTH = 900;
+        public static final int WINDOW_HEIGHT = 576;
+    }
+}
